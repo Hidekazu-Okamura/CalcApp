afa919aff5db37d63e91a593c3108843dc9486ae
Hidekazu-Okamura <d_conan_0505@yahoo.co.jp>
2022-06-07 20:28:00 +0900

    文字列としてデータ受取

--- app/src/main/java/jp/techacademy/hidekazu/okamura/calcapp/SecondActivity.kt
+++ app/src/main/java/jp/techacademy/hidekazu/okamura/calcapp/SecondActivity.kt
@@ -2,6 +2,7 @@ package jp.techacademy.hidekazu.okamura.calcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -10,9 +11,11 @@ class SecondActivity : AppCompatActivity() {
         setContentView(R.layout.activity_second)
 
         var ans: Double = 0.0
-        val value1 = intent.getIntExtra("VALUE1", 0)
-        val value2 = intent.getIntExtra("VALUE2", 0)
+        val value1 = intent.getStringExtra("VALUE1")
+        val value2 = intent.getStringExtra("VALUE2")
         val status = intent.getIntExtra("STATUS", 0)
+        Log.d("kotlintest", value1.toString())
+        Log.d("kotlintest", value2.toString())
         when ("$status") {
             1.toString() -> ans = value1.toDouble() + value2.toDouble()
             2.toString() -> ans = value1.toDouble() - value2.toDouble()
